223cd904f613f4be751b6feddbcc95a8126ca075
Mihail <mishca29954@mail.ru>
2023-06-01 10:29:54 +0300

    совмещен userPage и Auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,29 +1,35 @@
 import React, {useState} from 'react';
 import Loader from '../Components/UI/Loader/Loader';
+import UserPage from './UserPage';
 
 const Auth = () => {
   document.documentElement.scrollIntoView(true);
-  const [loadingPost, setLoadingPost] = useState(true);
-  setLoadingPost(true); //чтоб не ругался линтер
-  
+  const [loadingUserPage, setLoadingUserPage] = useState(true);
+  const [accessIsAllowed, setAccessIsAllowed] = useState(false);
   return (
     <div>
-      {loadingPost === false ?
-      <Loader />
+      {loadingUserPage === false ?
+        <Loader />
       : 
-      <div style={{fontSize: "30px", margin: "0 auto", display: "grid", justifyContent: "center"}}>
-          <p style={{color: "white", marginTop: "30px"}}>Регистрация в дипл</p>
-          <p style={{color: "white"}}>ФИО</p>
-          <input type="text" />
-          <p style={{color: "white"}}>Почта</p>
-          <input type="email" />
-          <p style={{color: "white"}}>Пароль</p>
-          <input type="password" />
-          <p style={{color: "white"}}>Подтверди пароль</p>
-          <input type="password" />
-          <p></p>
-        <button style={{marginTop: "30px"}} >Зарегестрироваться</button>
-      </div>
+      <>
+        {accessIsAllowed ?
+        <UserPage />
+        :
+        <div style={{fontSize: "30px", margin: "0 auto", display: "grid", justifyContent: "center"}}>
+            <p style={{color: "white", marginTop: "30px"}}>Регистрация в дипл</p>
+            <p style={{color: "white"}}>ФИО</p>
+            <input type="text" />
+            <p style={{color: "white"}}>Почта</p>
+            <input type="email" />
+            <p style={{color: "white"}}>Пароль</p>
+            <input type="password" />
+            <p style={{color: "white"}}>Подтверди пароль</p>
+            <input type="password" />
+            <p></p>
+          <button style={{marginTop: "30px", fontSize: "20px", borderRadius: "10px", background:"rgba(14, 0, 33, 1)", color: "white", border: "2px solid white", display: "flex", alignItems: "center", justifyContent: "center", height: "35px"}} onClick = {() => setAccessIsAllowed(true)}>Зарегестрироваться</button>
+        </div>
+        }
+        </>
       }
     </div>
   );
